537864141414979d17af9ce462c032977f40d4c1
samarth20131 <samrajgupta3110@gmail.com>
2022-08-09 22:55:41 +0530

    Practical 1 Que 1 & 2

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,9 +1,21 @@
 fun main(){
-    var x:Int = 100
-    val y:Int = 200
-    val z = 300.95F
-    println("x=$x")
-    println("y=$y")
-    println("z=$z")
+    val i: Int = 100
+    val c: Char= 'S'
+    val l = 20012021007L
+    val d = 2.1878152498
+    val f = 2.8975654f
+    val b: Boolean = true
+    val s: String="Sam"
+    val sv:Short=1245
+    val by:Byte=-128
 
+    println("Integer Value : $i")
+    println("Double Value : $d")
+    println("Float Value : $f")
+    println("Long Value : $l")
+    println("Short Value : $sv")
+    println("Byte Value : $by")
+    println("Character Value : $c")
+    println("Boolean Value : $b")
+    println("String Value : $s")
 }
---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val p:Int = 7
+    val q = p.toDouble()
+    val r:String = "07"
+    val s = Integer.parseInt(r)
+    val t = r.toInt()
+    var u = r.toDouble()
+
+    println("Integer Value : $p")
+    println("Double Value (From Integer) : $q")
+    println("String Value : $r")
+    println("Integer Value1 (From String) : $s")
+    println("Integer Value1 (From String) : $t")
+    println("Double Value (From String) : $u")
+}
